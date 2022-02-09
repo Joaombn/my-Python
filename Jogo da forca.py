714047c694256fb518e212c46d337e5dee135bd5
Joaombn <99299662+Joaombn@users.noreply.github.com>
2022-02-08 21:57:47 -0300

    Update Jogo da forca.py

--- Jogo da forca.py
+++ Jogo da forca.py
@@ -7,12 +7,30 @@ x = 0
 y = 0
 z = 0
 
+
 palavra = input("digite uma palavra ")
 Dica1 = input("digite a primeira dica ")
 Dica2 = input("digite a segunda dica ")
 Dica3 = input("digite a terceira dica ")
 controle = []
 acertos = []
+tents=0
+Tentativas=[]
+
+def topo():
+ print("\n", c * 30, "\n", "Jogo da forca 1.0".center(30), "\n", c * 30, "\nDica 1 -" + Dica1, "\nDica 2 -" + Dica2,"\nDica 3 -" + Dica3,"\n")
+
+def tentar():
+ 
+ global b
+ global tents
+ global Tentativas
+ 
+ b = input("\nDigite uma letra: ")
+ b = b.upper()
+ Tentativas.extend(b)
+ tents += 1
+ 
 
 os.system('clear')
 
@@ -26,15 +44,15 @@ full = 0
 
 while (full != 1):
 
-    print("\n", c * 30, "\n", "Jogo da forca 1.0".center(30), "\n", c * 30, "\nDica 1 -" + Dica1, "\nDica 2 -" + Dica2,
-          "\nDica 3 -" + Dica3)
+    topo()
     x = 0
     while (x < len(palavra)):
-        print(acertos[x])
-        x = x + 1
+     print(acertos[x])
+     x = x + 1
+    print()
 
-    b = input("\nDigite uma letra: ")
-    b = b.upper()
+    print("Tentativas:",Tentativas,tents,"\n","A!=Ã")
+    tentar()
     x = 0
 
     while (x < len(palavra)):
@@ -48,8 +66,20 @@ while (full != 1):
     if (acertos == controle):
         full = 1
 x = 0
+
+topo()
 while (x < len(palavra)):
     print(acertos[x])
     x = x + 1
 
+print()
 print("Parabéns Você venceu :)")
+print("Tentativas:", tents)
+print("Tentativas:", Tentativas)
+print("Obrigado por jogar :)")
+
+
+
+
+
+###
